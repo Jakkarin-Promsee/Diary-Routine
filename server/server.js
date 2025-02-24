da167a4686c7a522db3d9783dc6e7f1712172f0e
Night <jakkrinmes@gmail.com>
2025-02-24 14:57:51 +0700

    build: add static folder to public

--- server/server.js
+++ server/server.js
@@ -1,34 +1,9 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const mongoose = require('mongoose');
-const path = require("path");
+const app = require('./src/app.js');
+const connectDB = require('./src/config/db.js');
 
-const authRoutes = require('./src/routes/auth.routes.js');
-
-const app = express();
-
-// Middleware
-app.use(express.json());
-app.use(cors());
-app.use(helmet());
-
-// Serve static files from the "public" folder
-app.use(express.static(path.join(__dirname, "public")));
-
-// API Routes
-app.use('/api/auth', authRoutes);
-
-// 404 Middleware 
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Error 404 - Page Not Found' });
-});
-
-// Database Connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error('MongoDB Connection Error:', err));
+// Connect to Database
+connectDB();
 
 // Start Server
 const PORT = process.env.PORT || 5000;
