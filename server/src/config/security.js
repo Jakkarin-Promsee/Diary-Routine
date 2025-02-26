f5c9de5aaa23a0080df369532292b58897d9c4d7
Night <jakkrinmes@gmail.com>
2025-02-26 15:30:21 +0700

    fix: add connection link in sercurity

--- server/src/config/security.js
+++ server/src/config/security.js
@@ -3,7 +3,17 @@ const helmet = require('helmet');
 
 const configureSecurity = (app) => {
     app.use(cors());
-    app.use(helmet());
+    app.use(helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                connectSrc: ["'self'", "https://drt-api.vercel.app"],
+                scriptSrc: ["'self'", "'unsafe-inline'"],
+                imgSrc: ["'self'", "data:"],
+                frameAncestors: ["'self'"]
+            }
+        }
+    }));
 }
 
 module.exports = configureSecurity;
